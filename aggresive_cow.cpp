8d03e22ef1c458f0bfafeb08dced1bb7d63c753c
Nakul Bharti <32031946+knakul853@users.noreply.github.com>
2018-08-14 12:09:19 -0400

    Update aggresive_cow.cpp

--- aggresive_cow.cpp
+++ aggresive_cow.cpp
@@ -1,9 +1,16 @@
+/*
+author:nakul bharti
+date:2018-08-09 12:39:55.
+spoj
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 int main()
 {
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);   // fast i/o
     cin.tie(0);
     cout.tie(0);
     int t;
@@ -12,18 +19,18 @@ int main()
     {
         ll n,c;
         cin>>n>>c;
-        ll a[n];
+        ll a[n];        // all stall position
         for(ll i=0;i<n;i++)
         {
             cin>>a[i];
         }
 
-        sort(a,a+n);
-        ll beg = 0;
-        ll en = a[n-1];
+        sort(a,a+n);            //sorting the stall position
+        ll beg = 0;            //the minimum value difference possible in stall (keep in mind that min. diff. is our answer)
+        ll en = a[n-1];      ////the max value difference possible in stall
         ll  p = c;
-        ll ans = -1;
-        while(beg<en)
+        ll ans = -1;   //ans will contain our answer
+        while(beg<en) // simple binary search
         {
             ll mid = (beg+en)/2;
 
