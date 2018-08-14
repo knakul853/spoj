fe6832b42db60fd049f20d76e47257f65002dfb0
Nakul Bharti <32031946+knakul853@users.noreply.github.com>
2018-08-14 12:14:54 -0400

    prime_generator
    this problem uses segmented seive concept

---
+++ prime_generator.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include<cstdio>
+#include<math.h>
+#define ll long long
+#define size1 32000
+#include<vector>
+void setprime();
+void printprime(int a,int b);
+int p[size1];
+void setprime(){
+int temp=sqrt(size1);
+int k=0;
+for(int i=2;i<=size1;i++){
+
+        int x=sqrt(i);
+       int flag=0;
+        for(int j=2;j<=x;j++){
+
+            if((i%j)==0){
+                    flag=1;
+            break;
+
+            }
+        }
+        if(flag==0){
+            p[k++]=i;
+                }
+}
+}
+
+
+void printprime(int a,int b){
+    int f;
+    setprime();
+
+      for(int i=a;i<=b;i++){
+            f=0;
+        for(int j=0;p[j]<=sqrt(i);j++){
+              if((i%p[j])==0){
+                f=1;break;
+              }
+
+        }
+      if(f==0 &&i!=1){
+        printf("%d\n",i);
+      }
+
+      }
+}
+using namespace std;
+int main() {
+int n;
+scanf("%d",&n);
+while(n--){
+int a,b,f;
+scanf("%d %d",&a,&b);
+printprime(a,b);
+
+}
+	return 0;
+}
